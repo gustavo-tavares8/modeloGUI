9843e51f0aebe592e0397f319bedb0b968726b74
unknown <GUSTAVO_4943@UIRAPURU.BR>
2026-09-21 16:10:05 -0300

    coloquei o banco de dados num arquivo normal

--- Conexao.java
+++ Conexao.java
@@ -1,12 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-/**
- * Classe responsável por abrir a conexão com o banco de dados MySQL.
- *
- * IMPORTANTE: troque "root" e "1234" pelo usuário e senha do SEU MySQL local,
- * e confirme que o banco "cadastro" já foi criado (veja o script SQL no README).
- */
 public class Conexao {
 
     public static Connection getConnection() {
